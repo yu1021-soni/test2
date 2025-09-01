aea826969ec6de2d31f8804edbf157c7b9cd316f
yu <yuukasonia1021@gmail.com>
2025-09-01 17:35:30 +0900

    register.blade.phpまで

---
+++ src/database/migrations/2025_09_01_104502_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('item_img_url');
+            $table->unsignedInteger('price');
+            $table->text('description');
+            $table->tinyInteger('condition')->comment('1:良好,2:目立った傷や汚れなし,3:やや傷や汚れあり,4:状態が悪い');
+            $table->string('brand')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
---
+++ src/resources/views/auth/register.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth/register.css')}}">
+@endsection
+
+@section('content')
+<div class="register-form__content">
+
+  <form action="/register" class="form" method="post">
+  @csrf
+    <div class="register__group">
+      <div class="register__group-title">
+        <span class="register__label--item">ユーザ名</span>
+      </div>
+      <div class="register__group-content">
+      <div class="register__input--text">
+        <input type="text" name="name">
+      </div>
+      <div class="register__error">
+        @error('name')
+          {{ $message }}
+        @enderror
+      </div>
+    </div>
+    </div>
+      <div class="register__group">
+        <div class="register__group-title">
+          <span class="register__label--item">メールアドレス</span>
+        </div>
+        <div class="register__group-content">
+          <div class="register__input--text">
+            <input type="text" name="email">
+          </div>
+          <div class="register__error">
+            @error('email')
+              {{ $message }}
+            @enderror
+          </div>
+        </div>
+      </div>
+      <div class="register__group">
+        <div class="register__group-title">
+          <span class="register__label--item">パスワード</span>
+        </div>
+        <div class="register__group-content">
+          <div class="register__input--text">
+            <input type="password" name="password">
+          </div>
+          <div class="register__error">
+            @error('password')
+              {{ $message }}
+            @enderror
+          </div>
+        </div>
+      </div>
+      <div class="register__group">
+        <div class="register__group-title">
+          <span class="register__label--item">もう一度入力</span>
+        </div>
+        <div class="register__group-content">
+          <div class="register__input--text">
+            <input type="password" name="password_confirmation">
+          </div>
+          <div class="register__error">
+            @error('password')
+              {{ $message }}
+            @enderror
+          </div>
+        </div>
+      </div>
+      <div class="register__button">
+        <button class="register__button-submit" type="submit">登録</button>
+      </div>
+    </form>
+
+    <a class="header__link" href="/login">login</a>
+    
+</div>
