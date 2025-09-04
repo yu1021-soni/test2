cf8ac42845a0e8dd55115b5119e5996c9faa4e8d
yu <yuukasonia1021@gmail.com>
2025-09-04 11:39:48 +0900

    ログインバリデーション

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,8 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+
+//ログアウト
 use Laravel\Fortify\Contracts\LogoutResponse;
 use App\Http\Responses\LogoutResponse as CustomLogoutResponse;
+
+//ログインバリデーション
+use Laravel\Fortify\Http\Requests\LoginRequest as FortifyLoginRequest;
+use App\Http\Requests\LoginRequest as MyLoginRequest;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(LogoutResponse::class, CustomLogoutResponse::class);
+
+        // 既定の Fortify LoginRequest → 自作の FormRequest に差し替え
+        $this->app->bind(FortifyLoginRequestContract::class, MyLoginRequest::class);
     }
 
     /**
