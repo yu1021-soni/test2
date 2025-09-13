c9d8da6ff4d2390d78914aade0d57d3852f9953e
yu <yuukasonia1021@gmail.com>
2025-09-13 16:58:49 +0900

    common.css

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -10,14 +10,34 @@
 <body>
     <div class="app">
         <header class="header">
-            <div class="header__heading">
-                <img src="{{ asset('img/logo.svg') }}" alt="COACHTECH" class="header__logo">
-                @yield('link')
+            <div class="header__inner">
+                <a href="" class="header__logo-link">
+                    <img src="{{ asset('img/logo.svg') }}" alt="COACHTECH" class="header__logo">
+                </a>
+
+                <form class="search" action="/search" method="get">
+                    <input class="search__input" type="text" name="keyword" placeholder="なにをお探しですか？" value="{{ request('keyword') }}" >
+                </form>
+
+                <nav class="header__nav">
+                    <ul class="header__menu">
+                        <li>
+                            <form method="post" action="{{ route('logout') }}">
+                                @csrf
+                                <button type="submit" class="logout">ログアウト</button>
+                            </form>
+                        </li>
+                        <li><a href="/mypage" class="mypage">マイページ</a></li>
+                        <li><a href="/sell" class="sell">出品</a></li>
+                    </ul>
+                </nav>
             </div>
         </header>
+        
         <div class="content">
             @yield('content')
         </div>
+
     </div>
 </body>
 </html>
