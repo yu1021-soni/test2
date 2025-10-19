7a7d101c215f93c39b2f6235723cde7cf69e366d
yu <yuukasonia1021@gmail.com>
2025-10-19 13:02:40 +0900

    address BEM修正

--- src/resources/views/address.blade.php
+++ src/resources/views/address.blade.php
@@ -9,23 +9,22 @@
 @endsection
 
 @section('content')
-    <div class="content">
-        <h2>住所の変更</h2>
-        <form method="post" action="{{ route('address.change') }}">
+    <div class="address">
+        <h2 class="address__title">住所の変更</h2>
+        <form method="post" action="{{ route('address.change') }}" class="address__form">
         @csrf
-            {{-- purchase で保存済みの item_id を hidden で渡す --}}
             <input type="hidden" name="item_id" value="{{ session('checkout.item_id') }}">
 
-            <label>郵便番号</label>
-            <input type="text" name="postcode" value="{{ old('postcode') }}">
+            <label class="address__label">郵便番号</label>
+            <input class="address__input" type="text" name="postcode" value="{{ old('postcode') }}">
 
-            <label>住所</label>
-            <input type="text" name="address" value="{{ old('address') }}">
+            <label class="address__label">住所</label>
+            <input class="address__input" type="text" name="address" value="{{ old('address') }}">
 
-            <label>建物名</label>
-            <input type="text" name="building" value="{{ old('building') }}">
+            <label class="address__label">建物名</label>
+            <input class="address__input" type="text" name="building" value="{{ old('building') }}">
 
-            <button type="submit">更新する</button>
+            <button class="address__submit" type="submit">更新する</button>
         </form>
     </div>
 @endsection
