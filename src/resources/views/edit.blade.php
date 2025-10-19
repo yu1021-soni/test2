49fc39bcd10314ef42fbb3f9eda739c68b2c3d3c
yu <yuukasonia1021@gmail.com>
2025-10-19 13:33:25 +0900

    edit BEM修正

--- src/resources/views/edit.blade.php
+++ src/resources/views/edit.blade.php
@@ -9,50 +9,50 @@
 @endsection
 
 @section('content')
-    <div class="content">
-        <h2 class="content__title">プロフィール設定</h2>
-        <form action="{{ route('profile.update') }}" method="post" class="update" enctype="multipart/form-data">
+    <div class="profile">
+        <h2 class="profile__title">プロフィール設定</h2>
+        <form action="{{ route('profile.update') }}" method="post" class="profile__form" enctype="multipart/form-data">
             @csrf
-        <div class="user__img">
+        <div class="profile__image">
             <img src="{{ $user->user_img_url
             ? Storage::url($user->user_img_url)
-            : asset('img/avatar-default.png') }}" class="user__item" class="user__item"/>
-            <input id="user-img" class="uploader__input" type="file" name="user_img_url" accept=".jpg,.jpeg,.png,image/jpeg,image/png">
-            <label for="user-img" class="uploader__button">画像を選択する</label>
+            : asset('img/avatar-default.png') }}" class="profile__avatar"/>
+            <input id="user-img" class="profile__uploader-input" type="file" name="user_img_url" accept=".jpg,.jpeg,.png,image/jpeg,image/png">
+            <label for="user-img" class="profile__uploader-button">画像を選択する</label>
         </div>
     
-            <div class="profile">
+            <div class="profile__fields">
                 <div class="profile__name">
-                    <p class="title">ユーザ名</p>
-                    <input type="text" name="name" value="{{ $user->name }}" class="text">
+                    <p class="profile__label">ユーザ名</p>
+                    <input type="text" name="name" value="{{ $user->name }}" class="profile__input">
                     @error('name')
-                        <div class="form-error">{{ $message }}</div>
+                        <div class="profile__error">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="profile__postcode">
-                    <p class="title">郵便番号</p>
-                    <input type="text" name="postcode" value="{{ $user->postcode }}" class="text">
+                    <p class="profile__label">郵便番号</p>
+                    <input type="text" name="postcode" value="{{ $user->postcode }}" class="profile__input">
                     @error('postcode')
-                        <div class="form-error">{{ $message }}</div>
+                        <div class="profile__error">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="profile__address">
-                    <p class="title">住所</p>
-                    <input type="text" name="address" value="{{ $user->address }}" class="text">
+                    <p class="profile__label">住所</p>
+                    <input type="text" name="address" value="{{ $user->address }}" class="profile__input">
                     @error('address')
-                        <div class="form-error">{{ $message }}</div>
+                        <div class="profile__error">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="profile__building">
-                    <p class="title">建物名</p>
-                    <input type="text" name="building" value="{{ $user->building }}" class="text">
+                    <p class="profile__label">建物名</p>
+                    <input type="text" name="building" value="{{ $user->building }}" class="profile__input">
                     @error('building')
-                        <div class="form-error">{{ $message }}</div>
+                        <div class="profile__error">{{ $message }}</div>
                     @enderror
                 </div>
             </div>
-            <div class="submit">
-                <button type="submit" class="button__update">更新する</button>
+            <div class="profile__actions">
+                <button type="submit" class="profile__submit">更新する</button>
             </div>
         </form>
     </div>
