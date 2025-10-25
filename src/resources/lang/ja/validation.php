dc3e79af9dad25e9fb5c49a9a2bb31bf9bd478ba
yu <yuukasonia1021@gmail.com>
2025-10-25 20:30:42 +0900

    request 基本設計書確認

--- src/resources/lang/ja/validation.php
+++ src/resources/lang/ja/validation.php
@@ -1,9 +1,21 @@
 <?php
 return [
-    'required' => ':attribute を入力してください',
-    'email'    => ':attribute はメール形式で入力してください',
+    'required'  => ':attribute は必須です。',
+    'email'     => ':attribute はメール形式で入力してください。',
+    'confirmed' => ':attribute と一致しません。',
+    'unique'    => 'その :attribute は既に使用されています。',
+    'min' => [
+        'string' => ':attribute は:min文字以上で入力してください。',
+    ],
+    'max' => [
+        'string' => ':attribute は:max文字以内で入力してください。',
+    ],
+
+    // 項目名（ここが表示名になる）
     'attributes' => [
-        'email'    => 'メールアドレス',
-        'password' => 'パスワード',
+        'name'                  => 'ユーザー名',
+        'email'                 => 'メールアドレス',
+        'password'              => 'パスワード',
+        'password_confirmation' => 'パスワード（確認）',
     ],
 ];
